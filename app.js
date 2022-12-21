d7ed7380c1fda28954c66c8a86ff2dc739e44efa
TamaraTET <ebierethoro@gmail.com>
2022-12-22 00:47:40 +0100

    Chuck Norris Joke Generator app

---
+++ app.js
@@ -0,0 +1,30 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+  const number = document.querySelector('input[type="number"]').value;
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', `https://api.chucknorris.io/jokes/random`, true);
+
+
+  xhr.onload = function () {
+    if(this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      console.log(response);
+      let output = '';
+
+      if(response.id != '') {
+        output += `<li>${response.value}</li>`
+    } else {
+      output += '<li>Something went wrong</li>';
+}
+
+      document.querySelector('.jokes').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+
+  e.preventDefault();
+}
